500f92ef2f3f43d8262e9f121633435c04f2e2fe
Greg Sparks <gsparks@westmont.edu>
2019-04-04 21:53:46 -0700

    Melted hour data for a bar chart

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
+library(reshape2)
+library(dplyr)
 
 
 header <- dashboardHeader(title = "Homeplate Volunteer Dashboard")
@@ -28,6 +30,8 @@ ui <- dashboardPage(
 server <- function(input, output) {
   source('get_volunteer_data.R')
   volunteer_data <- get_volunteer_data()
+  hour_data <- volunteer_data %>% select(name, hours_2018, current_year_hours, total_hours)
+  melted_hours <- melt(data = hour_data, id.vars = "name", measure.vars = c('hours_2018', 'current_year_hours', 'total_hours'))
 
    output$hourDist <- renderPlot({
      ggplot(volunteer_data, aes(total_hours)) +
