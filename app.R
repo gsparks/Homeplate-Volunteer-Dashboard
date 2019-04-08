36ad1785069d9da186318a2a02d77381535616fa
Greg Sparks <gsparks@westmont.edu>
2019-04-07 18:22:00 -0700

    Added labels to bar plot

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(shinydashboard)
 library(ggplot2)
 library(reshape2)
 library(dplyr)
+library(ggthemes)
 
 
 header <- dashboardHeader(title = "Homeplate Volunteer Dashboard")
@@ -17,7 +18,7 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
   fluidRow(
-    box(plotOutput("hourDist"))
+    box(plotOutput("hourBar"))
   )
 )
 
@@ -28,17 +29,28 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output) {
+  # Pull Volunteer Data
+  # Once when the server starts
   source('get_volunteer_data.R')
   volunteer_data <- get_volunteer_data()
   hour_data <- volunteer_data %>% select(name, hours_2018, current_year_hours, total_hours)
-  melted_hours <- melt(data = hour_data, id.vars = "name", measure.vars = c('hours_2018', 'current_year_hours', 'total_hours'))
+  melted_hours <- melt(data = hour_data,
+                       id.vars = "name",
+                       measure.vars = c('hours_2018', 'current_year_hours', 'total_hours'))
 
-   output$hourDist <- renderPlot({
-     ggplot(volunteer_data, aes(total_hours)) +
-       geom_bar()
+  output$hourBar <- renderPlot({
+    ggplot(data=melted_hours, aes(x=variable, y=value, label=value)) +
+      geom_bar(stat="identity") +
+      labs(title="Homeplate Volunteer Hours", y="Hours", x="Year", caption="Hours updated daily.") +
+      theme_economist() +
+      theme(plot.title = element_text(hjust = 0.5)) +
+      stat_summary(aes(label = ..y..),
+                       fun.y = 'sum',
+                       geom = 'text',
+                       col = 'white',
+                       vjust = 1.5)
    })
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
